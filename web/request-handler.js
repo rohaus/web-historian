0ffeac2ef3858e3e3ecb9ca429c80aed8176ba64
Hack Reactor Students <github@hackreactor.com>
2013-10-17 17:24:57 -0700

    Implements user interface functionality

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,6 +1,11 @@
 var path = require('path');
 var fs = require('fs');
 var mysql = require('mysql');
+var url = require('url');
+var redirect = require('response-redirect');
+var http = require('http');
+
+http.ServerResponse.prototype.redirect = redirect;
 
 var datadir = module.exports.datadir = path.join(__dirname, "../data/sites.txt"); // tests will need to override this.
 
@@ -20,8 +25,8 @@ var headers = {
 };
 
 var contains = function(array, target){
-  for ( var j = 0; j < array.length; j++ ){
-    if ( array[j] === target ){
+  for( var j = 0; j < array.length; j++ ){
+    if( array[j] === target ){
       return true;
     }
   }
@@ -35,39 +40,48 @@ var sendResponse = module.exports.sendResponse = function(response, message, sta
 };
 
 module.exports.handleRequest = function (req, res) {
-  var msg;
+  var responseBody, pathname;
+  var parsedURL = url.parse(req.url);
   var archive = fs.readFileSync(datadir, "utf8").split("\n");
-  for ( var i = 0; i < archive.length-1; i++ ){
+  for( var i = 0; i < archive.length-1; i++ ){
     archive[i] = "/" + archive[i];
   }
   archive.pop();
-  if ( req.url === "/" || contains(archive, req.url) ){
-    if (req.method === "GET"){
-      if ( req.url === "/" ){
-        msg = "<input></input>";
-      }else {
-        msg = req.url;
+  if( req.url === "/" || contains(archive, parsedURL.pathname) ){
+    if( req.method === "GET" ){
+      if( req.url === "/" ){
+        pathname = path.join(__dirname, "/public/index.html");
+      }else{
+        pathname = path.join(__dirname, "../data/sites", parsedURL.pathname);
       }
-      sendResponse(res, msg);
-    }else if (req.method === "POST"){
+      responseBody = fs.readFileSync(pathname, "utf8");
+      sendResponse(res, responseBody);
+    }else if( req.method === "POST" ){
       var requestBody = "";
       req.on('data', function(chunk){
         requestBody += chunk;
       });
       req.on('end', function(){
-        fs.appendFileSync(datadir, requestBody+"\n");
+        requestBody = requestBody.split("=")[1];
         connection.connect(function(err){
           console.log("Connection error:",err);
         });
         var sqlCommand = "INSERT into urls (url, createdAt) values('"+requestBody+"', CURDATE());";
         connection.query(sqlCommand, function(errs, rows, fields){
-            console.log("SQL query error:",errs);
-          });
+          console.log("SQL query error:",errs);
+        });
         connection.end();
-        sendResponse(res, requestBody, 302);
+        if( !contains(archive, "/"+requestBody) ){
+          fs.appendFileSync(datadir, requestBody+"\n");
+          pathname = path.join(__dirname, "/public/redirect.html");
+          responseBody = fs.readFileSync(pathname, "utf8");
+          sendResponse(res, responseBody);
+        }else{
+          res.redirect(302, requestBody);
+        }
       });
     }
-  }else {
+  }else{
     sendResponse(res, "URL does not exist.", 404);
   }
 };
